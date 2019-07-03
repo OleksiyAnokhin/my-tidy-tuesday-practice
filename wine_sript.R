7719cc8c2503fdcecc794b764ade33f14c1b5d8e
Oleksiy Anokhin <oleksiy.anokhin@gmail.com>
2019-07-02 22:27:36 -0400

    Added media franchise revenues dataset

--- wine_sript.R
+++ wine_sript.R
@@ -23,22 +23,8 @@ library(plotly)
 wine_data <- readRDS("wine_data.rds")
 
 # Show correlation
-ggplot(wine_data, aes(points, price)) + geom_point() + theme_tufte()
+ggplot(wine_data, aes(points, price)) + geom_point()
 
-wine_by_country <- wine_data %>% group_by(country) %>% summarize(n()) %>% arrange(desc(`n()`)) # %>%
-  # ggplot(., aes(country, `n()`)) + geom_bar(stat = "identity")
-
-knitr::kable(wine_by_country)
-
-# wine_by_country_top10 <- wine_data %>% group_by(country) %>% summarize(n()) %>% 
-#   arrange(desc(`n()`)) %>% top_n(10) %>%
-#   ggplot(., aes(country, price)) + 
-#   geom_boxplot(color = "red", fill = "orange", alpha = 0.2)
-
-
-wine_data %>%
-  ggplot(., aes(country, price)) + 
-  geom_boxplot(color = "black", alpha = 0.2) + 
-  theme_tufte() + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+ggplotly(ggplot(wine_data, aes(points, price)) + geom_point()) + theme_tufte()
 
 
